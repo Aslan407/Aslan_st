f5b8b05efc37437e0f077712d18e741cc9df23ac
Aslan <aslan.myrzagaliev@mail.ru>
2022-09-21 14:08:06 +0600

    New project

--- aslan.js
+++ aslan.js
@@ -1,2 +1,21 @@
 "use strict";
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотели?', '');
+console.log(numberOfFilms);
 
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: true
+};
+
+const a = prompt('Один из последних просморенных фильмов', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из последних просморенных фильмов', ''),
+      d = prompt('На сколько оцените его', '');
+
+
+   personalMovieDB.movies[a] = b;
+   personalMovieDB.movies[c] = d;
+   console.log(personalMovieDB);
